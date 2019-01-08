210c606c89f16148e9cdb7ae6f2d4cf7d063e835
SpringNyan <springnyan@gmail.com>
2019-01-08 21:37:34 +0800

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,14 @@
 export default {
   input: "./lib/index.js",
-  output: {
-    file: "./dist/redux-advanced.js",
-    format: "esm"
-  },
+  output: [
+    {
+      file: "./dist/redux-advanced.js",
+      format: "cjs"
+    },
+    {
+      file: "./dist/redux-advanced.esm.js",
+      format: "esm"
+    }
+  ],
   external: ["redux", "redux-observable", "rxjs", "rxjs/operators", "immer"]
 };
