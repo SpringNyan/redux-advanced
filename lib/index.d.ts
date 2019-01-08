9bc88132b13d38115ccc6bf54cfe1cc0e38800df
SpringNyan <springnyan@gmail.com>
2019-01-08 21:09:51 +0800

    update files in package.json

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Container, UseContainer } from "./container";
-export { toActionObservable } from "./effect";
-export { createModelBuilder, ExtractDependencies, ExtractModel, ExtractProps, Model } from "./model";
-export { ExtractState } from "./state";
-export { createAdvancedStore } from "./store";
