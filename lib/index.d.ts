5badfb7dee6b1ffdea658a9ddd6138565e9bdd32
SpringNyan <springnyan@gmail.com>
2019-07-31 19:28:55 +0800

    Revert "temp"
    This reverts commit 8e4251ad0430e295549c32a5b16daebf122f79de.

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { Action, ActionHelper, AnyAction } from "./action";
-export { ExtractArgs } from "./args";
-export { Container, GetContainer } from "./container";
-export { ExtractDependencies } from "./dependencies";
-export { createModelBuilder, ExtractModel, Model } from "./model";
-export { ExtractState } from "./state";
-export { init } from "./store";
