406e53ba76eed0a2c7ce0c033ab393cc5c4df37c
SpringNyan <springnyan@gmail.com>
2019-09-10 21:11:01 +0800

    commit

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ export { Action, ActionHelper, actionTypes, AnyAction } from "./action";
 export { ExtractArgs } from "./args";
 export { Container, GetContainer } from "./container";
 export { ExtractDependencies } from "./dependencies";
-export { createModelBuilder, ExtractModel, Model, ModelOptions } from "./model";
+export {
+  createModelBuilder,
+  ExtractModel,
+  Model,
+  ModelBuilder,
+  ModelOptions
+} from "./model";
 export { ExtractState } from "./state";
 export { init, ReduxAdvancedContext, ReduxAdvancedOptions } from "./store";
